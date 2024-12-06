452d86fcc8ca0012bb838581434232d0cdcbbfd5
Sergey <sergey.sviridyuk.41@gmail.com>
2024-12-06 18:44:04 +0700

    ci

--- src/UserService.API/Startup.cs
+++ src/UserService.API/Startup.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.IdentityModel.Tokens.Jwt;
